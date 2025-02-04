d16e91d6fe1e8406536ff77f8631e165e2a156cf
SUPRIYA SARKAR <68441106+supriyo1598@users.noreply.github.com>
2025-02-04 23:08:18 +0530

    Stopwatch

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -10,79 +10,95 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: ageCalculator(),
+      home: StopWatchScreen(),
     );
   }
 }
 
-class ageCalculator extends StatefulWidget {
-  _ageCalculatorState createState() => _ageCalculatorState();
+class StopWatchScreen extends StatefulWidget {
+  _StopWatchScreenState createState() => _StopWatchScreenState();
 }
 
-class _ageCalculatorState extends State<ageCalculator> {
-  final TextEditingController _dobController = TextEditingController();
-  String _result = '';
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  late Stopwatch _stopwatch;
+  late Timer _timer;
 
-  void _calculateAge() {
-    String dobText = _dobController.text;
-    if (dobText.isEmpty) {
+  String _etime = '00:00:00';
+
+  void initState() {
+    super.initState();
+    _stopwatch = Stopwatch();
+  }
+
+  void _startTimer() {
+    //Chnging the state in every Seccond
+    _timer = Timer.periodic(Duration(microseconds: 100), (timer) {
       setState(() {
-        _result = 'Please enter a valid dob';
+        _etime = _formatElapsedTime(_stopwatch.elapsed);
       });
-      return;
-    }
-    try {
-      DateTime dob = DateFormat('yyyy-MM-dd').parse(dobText);
-      DateTime today = DateTime.now();
+    });
+  }
 
-      int years = today.year - dob.year;
-      int months = today.month - dob.month;
-      int days = today.day - dob.day;
+  String _formatElapsedTime(Duration duration) {
+    String hours = duration.inHours.toString().padLeft(2, '0');
+    String min = (duration.inMinutes % 60).toString().padLeft(2, '0');
+    String sec = (duration.inSeconds % 60).toString().padLeft(2, '0');
 
-      if (days < 0) {
-        months--;
-        days += DateTime(today.year, today.month, 0).day;
-      }
-      if (months < 0) {
-        years--;
-        months += 12;
-      }
+    return '$hours:$min:$sec';
+  }
 
-      setState(() {
-        _result = 'Age: $years yeras,$months months,and $days days';
-      });
-    } catch (e) {
-      setState(() {
-        _result = 'Please enter the date in yyyy-mm-dd format';
-      });
-    }
+  void _startStopWatch() {
+    setState(() {
+      _stopwatch.start();
+    });
+    _startTimer();
+  }
+
+  void _stopStopWatch() {
+    setState(() {
+      _stopwatch.stop();
+    });
+    _timer.cancel();
+  }
+
+  void _resetStopWatch() {
+    setState(() {
+      _stopwatch.reset();
+      _etime = '00:00:00';
+    });
+    _timer.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text('Age Calculator')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
+      appBar: AppBar(title: Text('StopWatch')),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _dobController,
-              decoration: InputDecoration(
-                labelText: 'Enter the DOB',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.datetime,
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-                onPressed: _calculateAge, child: Text('Calculate Age')),
-            SizedBox(height: 16),
-            Text(
-              _result,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Text(_etime,
+                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold)),
+            SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: _stopwatch.isRunning ? null : _startStopWatch,
+                  child: Text('Start'),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: _stopwatch.isRunning ? _stopStopWatch : null,
+                  child: Text('Stop'),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: _resetStopWatch,
+                  child: Text('Reset'),
+                ),
+              ],
             )
           ],
         ),
